e085a3a90b6cd6ad85e38ec5f0dc9928c69c1dd6
박경탁 <qkrrudxkr77@naver.com>
2021-06-23 21:49:53 +0900

    no message

--- src/boj/level1/bj_10718.java
+++ src/boj/level1/bj_10718.java
@@ -1,8 +1,9 @@
 package boj.level1;
 
-public class bj_10758 {
-    
-    public static void main(String[] args) {
+public class bj_10718 {
 
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
     }
 }

1f1c108f24bd187b8b7b84494e9bf8a596cc2974
탁 <qkrrudxkr77@naver.com>
2021-08-12 07:48:00 +0900

    no message

--- src/programmers/level2/타겟넘버.java
+++ src/programmers/level2/타겟넘버.java
@@ -3,7 +3,6 @@ package programmers.level2;
 public class 타겟넘버 {
 
     static int dfsCnt = 0;
-    static int bfsCnt = 0;
 
     public static void main(String[] args) {
         int[] numbers = {1, 1, 1, 1, 1};
@@ -11,28 +10,19 @@ public class 타겟넘버 {
 
         타겟넘버 tn = new 타겟넘버();
 
-        tn.dfs(1, numbers, target, numbers[0]);
-        tn.dfs(1, numbers, target, -numbers[0]);
-
+        tn.dfs(0, numbers, target, 0);
         System.out.println("dfsCnt = " + dfsCnt);
-//        System.out.println("bfsCnt = " + bfsCnt);
     }
 
     public void dfs(int index, int[] numbers, int target, int sum) {
         if (index == numbers.length) {
-
             if (target == sum) {
                 dfsCnt++;
             }
-
             return;
         }
 
         dfs(index + 1, numbers, target, sum + numbers[index]);
         dfs(index + 1, numbers, target, sum - numbers[index]);
-    }
-
-    public void bfs() {
-
     }
 }

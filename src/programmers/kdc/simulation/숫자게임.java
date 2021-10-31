c704401ef12ef54f11fe4ef78309cf591ce47cc4
박경탁 <qkrrudxkr77@naver.com>
2021-11-01 06:39:48 +0900

    시뮬레이션 - 숫자게임

---
+++ src/programmers/kdc/simulation/숫자게임.java
@@ -0,0 +1,27 @@
+package programmers.kdc.simulation;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class 숫자게임 {
+
+  public static void main(String[] args) {
+    System.out.println(solution(new int[]{5, 1, 3, 7}, new int[]{2, 2, 6, 8}));
+  }
+
+  private static int solution(int[] A, int[] B) {
+    Arrays.sort(A);
+    Arrays.sort(B);
+    int index = B.length - 1;
+    int answer = 0;
+
+    for (int i = A.length - 1; i >= 0; i--) {
+      if (A[i] < B[index]) {
+        index--;
+        answer++;
+      }
+    }
+
+    return answer;
+  }
+}

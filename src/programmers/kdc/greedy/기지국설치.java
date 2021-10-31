616ccff24908ece22fedca1784b728b9a30127b3
박경탁 <qkrrudxkr77@naver.com>
2021-11-01 07:41:30 +0900

    그리디 - 기지국 설치 Lv.3

--- src/programmers/kdc/greedy/기지국설치.java
+++ src/programmers/kdc/greedy/기지국설치.java
@@ -1,4 +1,29 @@
 package programmers.kdc.greedy;
 
+import java.util.Arrays;
+
 public class 기지국설치 {
+
+  public static void main(String[] args) {
+//    System.out.println(solution(11, new int[]{4, 11}, 1));
+    System.out.println(solution(16, new int[]{9}, 2));
+  }
+
+  private static int solution(int n, int[] stations, int w) {
+    int answer = 0;
+    int position = 1;
+    int index = 0;
+
+    while (position <= n) {
+      if (index < stations.length && stations[index] - w <= position) {
+        position = stations[index] + w + 1;
+        index++;
+      } else {
+        answer++;
+        position += 2 * w + 1;
+      }
+    }
+
+    return answer;
+  }
 }

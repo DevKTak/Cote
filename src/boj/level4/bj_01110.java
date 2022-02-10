ec512d6926a3bdce37a80a0d3ab7814a17348ac6
탁 <qkrrudxkr77@naver.com>
2022-02-10 17:49:51 +0900

    no message

---
+++ src/boj/level4/bj_01110.java
@@ -0,0 +1,21 @@
+package boj.level4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj_01110 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int cnt = 0;
+
+        while (true) {
+
+        }
+
+    }
+}
--- src/programmers/kdc2/mockTest/b/test2.java
+++ src/programmers/kdc2/mockTest/b/test2.java
@@ -3,6 +3,6 @@ package programmers.kdc2.mockTest.b;
 public class test2 {
 
   public static void main(String[] args) {
-    System.out.println(solution());
+    System.out.println();
   }
 }

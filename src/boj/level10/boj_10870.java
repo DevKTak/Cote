8d1decc09f68def0e8d4aafef59b0f068b0ef5d9
ktpark <>
2022-02-14 07:51:54 +0900

    level10 진행중

---
+++ src/boj/level10/boj_10870.java
@@ -0,0 +1,9 @@
+package boj.level10;
+
+/** 피보나치 수 5 */
+public class boj_10870 {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/boj/level10/boj_10872.java
@@ -0,0 +1,18 @@
+package boj.level10;
+
+import java.util.Scanner;
+
+/** 팩토리얼 */
+public class boj_10872 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(factorial(sc.nextInt()));
+    }
+
+    private static int factorial(int N) {
+        if (N == 0) return 1;
+
+        return N * factorial(N - 1);
+    }
+}

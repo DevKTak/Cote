4ebeaf3b0e090cef7d679e2eac886debdbb35932
탁 <qkrrudxkr77@naver.com>
2022-02-14 16:51:25 +0900

    정렬 - 수 정렬하기

---
+++ src/boj/level12/boj_02750.java
@@ -0,0 +1,30 @@
+package boj.level12;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * 수 정렬하기
+ */
+public class boj_02750 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        for (int v : arr) {
+            System.out.println(v);
+        }
+    }
+}

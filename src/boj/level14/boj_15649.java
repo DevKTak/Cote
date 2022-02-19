e7a8453472f95f7565b5fb9f6a72287de9962610
ktpark <>
2022-02-20 00:15:29 +0900

    백트래킹 - N과 M (1)

---
+++ src/boj/level14/boj_15649.java
@@ -0,0 +1,63 @@
+package boj.level14;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_15649 {
+
+    public static int N;
+    public static int M;
+    public static int[] arr;
+    public static boolean[] visit;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken()); // 1 ~ N 까지 자연수 중
+        M = Integer.parseInt(st.nextToken()); // M 개를 고른다
+
+        arr = new int[M];
+        visit = new boolean[N];
+
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int depth) {
+        // 출력
+        if (depth == M) {
+            for (int v : arr) {
+                sb.append(v).append(" ");
+            }
+            sb.append(System.lineSeparator());
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visit[i]) { // 방문한 적이 없으면 (노드 중복방지)
+                visit[i] = true; //  방문처리
+                arr[depth] = i + 1;
+
+                dfs(depth + 1);
+
+                // 자식노드 방문이 끝나고 돌아오면 방문노드를 방문하지 않은 상태로 변경
+                visit[i] = false;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+

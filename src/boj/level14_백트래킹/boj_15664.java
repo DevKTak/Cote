311aba3e13f787b7ecd9aeae1432892ac975affa
탁 <qkrrudxkr77@naver.com>
2022-03-03 14:02:14 +0900

    no message

--- src/boj/level14_백트래킹/boj_15664.java
+++ src/boj/level14_백트래킹/boj_15664.java
@@ -8,42 +8,43 @@ import java.util.LinkedHashSet;
 import java.util.StringTokenizer;
 
 /**
- * N과 M (9)
+ * N과 M (10)
  */
-public class boj_15663 {
+public class boj_15664 {
 
+    private static int N, M;
     private static int[] arr, tempArr;
     private static boolean[] visited;
-    private static int N, M;
     private static StringBuilder sb = new StringBuilder();
-    private static LinkedHashSet<String> lhs = new LinkedHashSet<>(); // 삽입된 순서대로 중복 X
-
+    private static LinkedHashSet<String> lhs = new LinkedHashSet<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken()); // N개의 자연수
-        M = Integer.parseInt(st.nextToken()); // M개를 고름
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
         arr = new int[N];
         tempArr = new int[M];
         visited = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+
 
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
 
-        dfs(0);
+        dfs(0, 0);
 
         for (String s : lhs) {
             System.out.println(s);
         }
     }
 
-    private static void dfs(int depth) {
+    private static void dfs(int depth, int asc) {
 
         // Base condition
         if (depth == M) {
@@ -56,12 +57,12 @@ public class boj_15663 {
             return;
         }
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = asc; i < N; i++) {
             if (!visited[i]) {
                 visited[i] = true;
                 tempArr[depth] = arr[i];
 
-                dfs(depth + 1);
+                dfs(depth + 1, i + 1);
                 visited[i] = false;
             }
         }

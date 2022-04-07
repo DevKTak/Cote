36a9425de6b8548ba5076e3bf4feb0605b1b0d6a
ktpark <>
2022-04-07 21:30:11 +0900

    큐and덱 AC 진행중

---
+++ src/boj/level18_큐and덱/boj_05430_AC.java
@@ -0,0 +1,29 @@
+package boj.level18_큐and덱;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_05430_AC {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int T = Integer.parseInt(st.nextToken());
+
+        while (T-- > 0) {
+            for (int i = 0; i < 3; i++) {
+                st = new StringTokenizer(br.readLine());
+                String p = st.nextToken();
+
+                st = new StringTokenizer(br.readLine());
+                int n = Integer.parseInt(st.nextToken());
+
+
+
+            }
+        }
+    }
+}

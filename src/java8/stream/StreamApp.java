f7ebce39c908d49b6f7ec6685f587d275e8e0f4b
ktpark <>
2022-04-15 18:56:47 +0900

    스트림 공부

--- src/java8/stream/StreamApp.java
+++ src/java8/stream/StreamApp.java
@@ -3,6 +3,7 @@ package java8.stream;
 import java8.OnlineClass;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.function.Predicate;
@@ -19,18 +20,18 @@ public class StreamApp {
         springClasses.add(new OnlineClass(4, "spring core", false));
         springClasses.add(new OnlineClass(5, "rest api development", false));
 
-        System.out.println("spring 으로 시작하는 수업");
+        System.out.println("\nspring 으로 시작하는 수업");
         springClasses.stream()
                 .filter(onlineClass -> onlineClass.getTitle().startsWith("spring"))
                 .forEach(onlineClass -> System.out.println(onlineClass.getId()));
 
-        System.out.println("close 되지 않은 수업");
+        System.out.println("\nclose 되지 않은 수업");
         springClasses.stream()
 //                .filter(onlineClass -> !onlineClass.isClosed())
                 .filter(Predicate.not(OnlineClass::isClosed)) // 위와 같음
                 .forEach(onlineClass -> System.out.println(onlineClass.getId()));
 
-        System.out.println("수업 이름만 모아서 스트림 만들기");
+        System.out.println("\n수업 이름만 모아서 스트림 만들기");
         springClasses.stream()
 //                .map(onlineClass -> onlineClass.getTitle())
                 .map(OnlineClass::getTitle)
@@ -46,35 +47,54 @@ public class StreamApp {
         keesunEvents.add(springClasses);
         keesunEvents.add(javaClasses);
 
-        System.out.println("두 수업 목록에 들어있는 모든 수업 아이디 출력");
+        System.out.println("\n두 수업 목록에 들어있는 모든 수업 아이디 출력");
         keesunEvents.stream()
 //                .flatMap(list -> list.stream())
                 .flatMap(Collection::stream)
                 .forEach(onlineClass -> System.out.println(onlineClass.getId()));
 
-        System.out.println("10부터 1씩 증가하는 무제한 스트림 중에서 앞에 10개 빼고 최대 10 까지만");
+        System.out.println("\n10부터 1씩 증가하는 무제한 스트림 중에서 앞에 10개 빼고 최대 10 까지만");
         Stream.iterate(10, i -> i + 1)
                 .skip(10)
                 .limit(10)
                 .forEach(System.out::println);
 
-        System.out.println("자바 수업 중에 Test가 들어있는 수업이 있는지 확인");
+        System.out.println("\n자바 수업 중에 Test가 들어있는 수업이 있는지 확인");
         boolean test = javaClasses.stream()
                 .anyMatch(oc -> oc.getTitle().contains("Test"));
         System.out.println(test);
 
-        System.out.println("스프링 수업 중에 제목에 spring이 들어간 제목만 모아서 List로 만들기");
+        System.out.println("\n스프링 수업 중에 제목에 spring이 들어간 제목만 모아서 List로 만들기");
         List<String> spring = springClasses.stream()
                 .filter(oc -> oc.getTitle().contains("spring"))
                 .map(OnlineClass::getTitle)
                 .collect(Collectors.toList());
         spring.forEach(System.out::println);
 
-        System.out.println("자바 수업 중에 The Java가 포함된 수업의 id의 합");
+        System.out.println("\n자바 수업 중에 The Java가 포함된 수업의 id의 합");
         int sum = javaClasses.stream()
                 .filter(oc -> oc.getTitle().contains("The Java"))
                 .mapToInt(OnlineClass::getId)
                 .sum();
         System.out.println(sum);
+
+        System.out.println("\n Stream 배열 => int[] 반환");
+        int[] arr = {1, 2, 3};
+
+        int[] result1 = Arrays.stream(arr)
+                .map(v -> v + 10)
+                .toArray();
+        System.out.println(Arrays.toString(result1));
+
+        System.out.println("\n Stream 리스트 => String[] 반환");
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+
+        String[] result2 = list.stream()
+                .map(String::toUpperCase)
+                .toArray(String[]::new);
+        System.out.println(Arrays.toString(result2));
     }
 }

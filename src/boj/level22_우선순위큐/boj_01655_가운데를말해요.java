2a8c4bebb1c5d910c28af6e15c8aa844b81fb985
ktpark <>
2022-05-12 23:37:47 +0900

    우선순위큐 - 가운데를 말해요

--- src/boj/level22_우선순위큐/boj_01655_가운데를말해요.java
+++ src/boj/level22_우선순위큐/boj_01655_가운데를말해요.java
@@ -4,45 +4,39 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
 public class boj_01655_가운데를말해요 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> descPQ = new PriorityQueue<>((o1, o2) -> o2 - o1);
+        PriorityQueue<Integer> ascPQ = new PriorityQueue<>();
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
 
         while (N-- > 0) {
-            st = new StringTokenizer(br.readLine());
-            pq.offer(Integer.parseInt(st.nextToken()));
-            System.out.println(pq);
-//            Object[] pqArr = pq.toArray();
-//            int[] arr = new int[pqArr.length];
-//            for (int i = 0; i < pqArr.length; i++) {
-//                arr[i] = (int) pqArr[i];
-//            }
-//            System.out.println(Arrays.toString(arr));
-//            int arrLength = arr.length;
-//
-//            if (arrLength == 0) {
-//                sb.append(arr[0]).append(System.lineSeparator());
-//                continue;
-//            }
-//
-//            if (arrLength % 2 == 1) {
-//                sb.append(arr[arrLength / 2]).append(System.lineSeparator());
-//            } else {
-//                if (arr[arrLength / 2] > arr[arrLength / 2 - 1]) {
-//                    sb.append(arr[arrLength / 2 - 1]).append(System.lineSeparator());
-//                } else {
-//                    sb.append(arr[arrLength / 2]).append(System.lineSeparator());
-//                }
-//            }
+            int num = Integer.parseInt(br.readLine());
+
+            if (descPQ.size() == ascPQ.size()) {
+                if (!ascPQ.isEmpty() && ascPQ.peek() < num) {
+                    int temp = ascPQ.remove();
+                    ascPQ.offer(num);
+                    descPQ.offer(temp);
+                } else {
+                    descPQ.offer(num);
+                }
+            } else {
+                if (!descPQ.isEmpty() && descPQ.peek() > num) {
+                    int temp = descPQ.remove();
+                    descPQ.offer(num);
+                    ascPQ.offer(temp);
+                } else {
+                    ascPQ.offer(num);
+                }
+            }
+            sb.append(descPQ.peek()).append(System.lineSeparator());
         }
         System.out.print(sb);
     }

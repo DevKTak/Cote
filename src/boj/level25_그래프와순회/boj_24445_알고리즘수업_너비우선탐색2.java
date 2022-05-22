a6f48d2c4bb83dcb9ac0453ca1005d47a06765d5
ktpark <>
2022-05-22 17:44:38 +0900

    no message

--- src/boj/level25_그래프와순회/boj_24445_알고리즘수업_너비우선탐색2.java
+++ src/boj/level25_그래프와순회/boj_24445_알고리즘수업_너비우선탐색2.java
@@ -5,28 +5,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class boj_24480_알고리즘수업_깊이우선탐색2 {
+public class boj_24445_알고리즘수업_너비우선탐색2 {
 
     private static List<Integer>[] adList;
     private static boolean[] visited;
     private static int[] order;
     private static int cnt;
 
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken()); // 정점의 수
-        int M = Integer.parseInt(st.nextToken()); // 간선의 수
-        int R = Integer.parseInt(st.nextToken()); // 시작 정점
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
 
+        adList = new ArrayList[N + 1];
         visited = new boolean[N + 1];
         order = new int[N];
-        adList = new ArrayList[N + 1];
 
         for (int i = 1; i <= N; i++) {
-            adList[i] = new ArrayList();
+            adList[i] = new ArrayList<>();
         }
 
         while (M-- > 0) {
@@ -40,24 +39,28 @@ public class boj_24480_알고리즘수업_깊이우선탐색2 {
         }
 
         for (int i = 1; i <= N; i++) {
-            Collections.sort(adList[i], (o1, o2) -> o2 - o1);
+            Collections.sort(adList[i], ((o1, o2) -> o2 - o1));
         }
 
-        dfs(R);
+        bfs(R);
 
         Arrays.stream(order).forEach(System.out::println);
     }
 
-    private static void dfs(int R) {
-        if (visited[R]) {
-            return;
-        }
+    private static void bfs(int R) {
+        Queue<Integer> q = new LinkedList();
+        q.offer(R);
         visited[R] = true;
-        order[R - 1] = ++cnt;
 
-        for (int ad : adList[R]) {
-            if (!visited[ad]) {
-                dfs(ad);
+        while (!q.isEmpty()) {
+            int num = q.poll();
+            order[num - 1] = ++cnt;
+
+            for (int ad : adList[num]) {
+                if (!visited[ad]) {
+                    visited[ad] = true;
+                    q.offer(ad);
+                }
             }
         }
     }

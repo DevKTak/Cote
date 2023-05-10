a16a8b5f855a22ff80722c18711915111decb758
ktpark <qkrrudxkr77@naver.com>
2023-05-11 08:09:05 +0900

    no message

--- src/ndb/정렬/ndb_성적이낮은순서로학생출력하기.java
+++ src/ndb/정렬/ndb_성적이낮은순서로학생출력하기.java
@@ -1,6 +1,7 @@
 package ndb.정렬;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class ndb_성적이낮은순서로학생출력하기 {
 
@@ -8,7 +9,8 @@ public class ndb_성적이낮은순서로학생출력하기 {
         NameAndGrade hong = new NameAndGrade("홍길동", 95);
         NameAndGrade lee = new NameAndGrade("이순신", 77);
         NameAndGrade park = new NameAndGrade("박경탁", 100);
-        NameAndGrade[] arr = {hong, lee, park};
+        NameAndGrade park2 = new NameAndGrade("박경탁2", 100);
+        NameAndGrade[] arr = {hong, lee, park, park2};
 
 //        Arrays.sort(arr, (nameAndGrade1, nameAndGrade2) ->
 //           nameAndGrade1.getGrade() - nameAndGrade2.getGrade()
@@ -18,10 +20,13 @@ public class ndb_성적이낮은순서로학생출력하기 {
 //            System.out.println(nameAndGrade.getName());
 //        }
 
-        /** Comparator 구현 */
-//        Arrays.stream(arr)
-//                .sorted(Comparator.comparing(NameAndGrade::getGrade))
-//                .forEach(nameAndGrade -> System.out.println(nameAndGrade.getName()));
+        /** Comparator 구현 (성적 내림차순 + 이름 내림차순) */
+        Arrays.stream(arr)
+                .sorted(Comparator.comparing(NameAndGrade::getGrade).reversed()
+                .thenComparing(Comparator.comparing(NameAndGrade::getName).reversed()))
+                .forEach(nameAndGrade -> System.out.println(nameAndGrade.getName() + " " + nameAndGrade.getGrade()));
+
+        System.out.println();
 
         /** Comparable 구현 */
         Arrays.sort(arr);

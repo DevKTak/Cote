eaeaed6a7a71653107f1457ae48a8c8d0be87e0c
ktpark <qkrrudxkr77@naver.com>
2023-05-31 18:27:51 +0900

    no message

---
+++ src/ndb/그리디/ndb_모험가길드.java
@@ -0,0 +1,41 @@
+package ndb.그리디;
+
+import java.util.Arrays;
+
+public class ndb_모험가길드 {
+
+    public static void main(String[] args) {
+        int n = 5;
+//        Integer[] arr = {2, 3, 1, 2, 2};
+        Integer[] arr = {3, 2, 4, 4, 4};
+//        Integer[] arr = {6, 2, 1, 3, 2};
+
+        /* 나의풀이
+        Arrays.sort(arr, Comparator.reverseOrder());
+        int cnt = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            i += arr[i] - 1;
+            if (i < arr.length) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+         */
+
+        int cnt = 0; // 현재 그룹에 포함된 모험가의 수
+        int result = 0; // 총 그룹의 수
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) { // 공포도가 낮은 것부터
+            cnt++; // 현재 그룹에 모험가 포함
+
+            if (cnt >= arr[i]) { // 그룹에 포함된 모험가의 수가 현재의 공포도 이상이라면, 그룹 결성
+                result++; // 그룹 수 추가
+                cnt = 0; // 현재 그룹에 포함된 모함가의 수 초기화
+            }
+        }
+        System.out.println(result);
+    }
+}

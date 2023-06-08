2610925cd69527d9921d41ac1c7938c61ab2cddf
ktpark <qkrrudxkr77@naver.com>
2023-06-08 16:39:00 +0900

    no message

---
+++ src/ndb/그리디/ndb_만들수없는금액.java
@@ -0,0 +1,28 @@
+package ndb.그리디;
+
+import java.util.Arrays;
+
+public class ndb_만들수없는금액 {
+
+    private static final int n = 5;
+    private static final int[] arr = {3, 2, 1, 1, 9};
+//    private static final int[] arr = {3, 5, 7};
+
+    public static void main(String[] args) {
+        System.out.println(notMakePrice());
+    }
+
+    private static int notMakePrice() {
+        Arrays.sort(arr);
+        int minimum = 1; // 만들 수 없는 금액의 최솟값
+
+        for (int i = 0; i < n; i++) {
+          if (minimum < arr[i]) {
+              break;
+          }
+          minimum += arr[i];
+        }
+
+        return minimum;
+    }
+}

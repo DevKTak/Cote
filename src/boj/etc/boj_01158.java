6118860309f41807bfc4423ede8a4f8fe00fa4b1
ktpark <qkrrudxkr77@naver.com>
2023-07-18 00:07:39 +0900

    백준 요세푸스 문제

---
+++ src/boj/etc/boj_01158.java
@@ -0,0 +1,37 @@
+package boj.etc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+// 요세푸스 문제
+public class boj_01158 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken()); // 사람 수
+        int k = Integer.parseInt(st.nextToken()); // K 번째 사람 제거
+
+        StringBuilder sb = new StringBuilder();
+        List<Integer> list = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(i + 1);
+        }
+
+        int cur = 0;
+        while (!list.isEmpty()) {
+            cur = (cur + k - 1) % list.size();
+            sb.append(list.remove(cur));
+
+            if (list.size() != 0){
+                sb.append(", ");
+            }
+        }
+        System.out.println("<" + sb + ">");
+    }
+}

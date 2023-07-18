5682a1d88a51ac595c079840432c3a15dd1d9177
박경탁 <68748397+DevKTak@users.noreply.github.com>
2023-07-19 00:55:28 +0900

    [Silver IV] Title: 괄호, Time: 136 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -3,37 +3,42 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
- 
+
 public class Main {
 
-	public static String valid(String answerStr) {
-		int cnt = 0;
-		for (int i = 0; i < answerStr.length(); i++) {
-			if (answerStr.charAt(i) == '(') {
-				cnt++;
-			} else {
-				cnt--;
-			}
-			if (cnt < 0) {
-				return "NO";
-			}
-		}
-		if (cnt == 0) {
-			return "YES";
-		} else {
-			return "NO";
-		}
-	}
-	
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		while (N-- > 0) {
-			st = new StringTokenizer(br.readLine());
-			String answer = st.nextToken();
-			System.out.println(valid(answer));
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        Stack<Character> stack;
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            stack = new Stack<>();
+            st = new StringTokenizer(br.readLine());
+
+            char[] arr = st.nextToken().toCharArray();
+
+            System.out.println(solve(arr, stack));
+        }
+    }
+
+    private static String solve(char[] arr, Stack<Character> stack) {
+        for (char c : arr) {
+            if (c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) { // pop 할 원소가 없을 경우
+                    return "NO";
+                }
+                stack.pop();
+            }
+        }
+
+        if (stack.isEmpty()) {
+            return "YES";
+        } else { // 여는괄호가 남은 경우
+            return "NO";
+        }
+    }
 }

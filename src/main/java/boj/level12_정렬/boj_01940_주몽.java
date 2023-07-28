f1d45efc5e08029b0e6cc6d92e00543790948cde
ktpark <qkrrudxkr77@naver.com>
2023-07-29 02:05:30 +0900

    no message

---
+++ src/main/java/boj/level12_정렬/boj_01940_주몽.java
@@ -0,0 +1,36 @@
+package boj.level12_정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+/**
+ * 주몽
+ */
+public class boj_01940_주몽 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        Map<Integer, Boolean> map = new HashMap<>();
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            map.put(Integer.parseInt(st.nextToken()), true);
+        }
+
+        int cnt = 0;
+        for (int key : map.keySet()) {
+            if (map.getOrDefault(M - key, false)) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt / 2);
+    }
+}

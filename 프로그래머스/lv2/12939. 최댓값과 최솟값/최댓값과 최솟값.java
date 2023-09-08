72563731b3d6aff31be60cfe742c611bc798b333
박경탁 <68748397+DevKTak@users.noreply.github.com>
2023-09-09 00:02:59 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 11.99 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -2,9 +2,26 @@ import java.util.*;
 
 class Solution {
     public String solution(String s) {
-		String[] arr = s.split(" ");
-		Arrays.sort(arr, Comparator.comparingInt(Integer::parseInt));
+// 		String[] arr = s.split(" ");
+// 		Arrays.sort(arr, Comparator.comparingInt(Integer::parseInt));
 
-		return arr[0] + " " + arr[arr.length - 1];
+// 		return arr[0] + " " + arr[arr.length - 1];
+        
+           // 문자열을 공백으로 나누어 숫자 배열로 변환
+        String[] numbers = s.split(" ");
+        
+        // 초기 최소값과 최대값 설정
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        
+        // 숫자 배열을 순회하며 최소값과 최대값 업데이트
+        for (String numStr : numbers) {
+            int num = Integer.parseInt(numStr);
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+        
+        // 결과 문자열 생성 후 반환
+        return min + " " + max;
     }
 }

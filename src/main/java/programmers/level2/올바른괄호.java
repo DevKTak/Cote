520df29d0fd1b7a2f58ff3730d84cf496a9bdddf
ktpark <qkrrudxkr77@naver.com>
2023-09-12 13:49:09 +0900

    feat: 코테 훈련

---
+++ src/main/java/programmers/level2/올바른괄호.java
@@ -0,0 +1,26 @@
+package programmers.level2;
+
+import java.util.Stack;
+
+public class 올바른괄호 {
+
+	public static void main(String[] args) {
+		System.out.println(solution(")()("));
+	}
+
+	private static boolean solution(String s) {
+		Stack<Character> stack = new Stack<>();
+
+		for (char c : s.toCharArray()) {
+			if (c == '(') {
+				stack.push(c);
+			} else {
+				if (stack.isEmpty() || stack.pop() == ')') {
+					return false;
+				}
+			}
+		}
+
+		return stack.size() <= 0;
+	}
+}

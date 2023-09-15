e1fa11445b3fd1666150c29f81e2896bb6ab888c
박경탁 <68748397+DevKTak@users.noreply.github.com>
2023-09-15 13:37:43 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -5,21 +5,21 @@ import java.util.List;
 
 class Solution {
     public int[] solution(int brown, int yellow) {
-    int[] answer = new int[2];
-        int total = brown + yellow; // 전체 격자 수
-        
-        // 가능한 가로 길이와 세로 길이의 조합을 찾기 위한 반복문
-        for (int x = 1; x <= total; x++) {
-            if (total % x == 0) {
-                int y = total / x;
-                if ((x + y) * 2 == brown + 4) { // 주어진 갈색 격자 수와 맞는지 확인
-                    answer[0] = y;
-                    answer[1] = x;
-                    break;
-                }
-            }
-        }
-        
-        return answer;
+ 	int[] result = new int[2];
+		int total = brown + yellow;
+
+		for (int col = 1; col <= Math.sqrt(total); col++) {
+			if (total % col == 0) {
+				int row = total / col;
+
+				if ((row - 2) * (col - 2) == yellow) {
+					result[0] = row;
+					result[1] = col;
+
+					break;
+				}
+			}
+		}
+		return result;
     }
 }

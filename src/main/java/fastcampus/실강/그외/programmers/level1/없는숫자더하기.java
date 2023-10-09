06f6b8d41b6da6d478f5c8e50bede5266620f700
ktpark <qkrrudxkr77@naver.com>
2023-10-09 23:03:12 +0900

    feat: 코테 훈련

--- src/main/java/fastcampus/실강/그외/programmers/level1/없는숫자더하기.java
+++ src/main/java/fastcampus/실강/그외/programmers/level1/없는숫자더하기.java
@@ -1,4 +1,27 @@
 package fastcampus.실강.그외.programmers.level1;
 
+import java.util.Arrays;
+
 public class 없는숫자더하기 {
+
+	public static void main(String[] args) {
+		없는숫자더하기 P = new 없는숫자더하기();
+		System.out.println(P.solution(new int[] {1, 2, 3, 4, 6, 7, 8, 0}));
+	}
+
+	public int solution(int[] numbers) {
+/*		int answer = 0;
+		Map<Integer, Boolean> map = new HashMap<>();
+
+		for (int n : numbers) {
+			map.put(n, true);
+		}
+		for (int i = 0; i <= 9; i++) {
+			if (map.get(i) == null) {
+				answer += i;
+			}
+		}
+		return answer;*/
+		return 45 - Arrays.stream(numbers).sum();
+	}
 }

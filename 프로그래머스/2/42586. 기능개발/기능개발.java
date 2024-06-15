669ddb79884460e0ba63391e6d88f1b41b6e7afe
박경탁 <68748397+DevKTak@users.noreply.github.com>
2024-06-15 16:20:07 +0900

    [level 2] Title: 기능개발, Time: 2.34 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+  List<Integer> daysList = new ArrayList<>();
+
+        for (int i = 0; i < progresses.length; i++) {
+            int remainWork = 100 - progresses[i];
+            int days = (int) Math.ceil((double) remainWork / speeds[i]);
+            daysList.add(days);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int deployDay = daysList.get(0);
+        int count = 1;
+
+        for (int i = 1; i < daysList.size(); i++) {
+            if (daysList.get(i) <= deployDay) {
+                count++;
+            } else {
+                result.add(count);
+                count = 1;
+                deployDay = daysList.get(i);
+            }
+        }
+        result.add(count);
+
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+}

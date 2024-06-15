afbd9ddb8b7a563c02754cacfa99991677fb7023
박경탁 <68748397+DevKTak@users.noreply.github.com>
2024-06-15 16:57:58 +0900

    [level 2] Title: 기능개발, Time: 2.15 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586#qna) 
 
 ### 성능 요약
 
-메모리: 78.9 MB, 시간: 2.34 ms
+메모리: 73.5 MB, 시간: 2.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 15일 16:20:05
+2024년 06월 15일 16:57:56
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,29 +2,29 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-  List<Integer> daysList = new ArrayList<>();
+     List<Integer> daysList = new ArrayList<>();
 
-        for (int i = 0; i < progresses.length; i++) {
-            int remainWork = 100 - progresses[i];
-            int days = (int) Math.ceil((double) remainWork / speeds[i]);
-            daysList.add(days);
-        }
+		for (int i = 0; i < progresses.length; i++) {
+			int remainWork = 100 - progresses[i];
+			int days = (int)Math.ceil((double)remainWork / speeds[i]);
+			daysList.add(days);
+		}
 
-        List<Integer> result = new ArrayList<>();
-        int deployDay = daysList.get(0);
-        int count = 1;
+		List<Integer> result = new ArrayList<>();
+		int deployDay = daysList.get(0);
+		int count = 1;
 
-        for (int i = 1; i < daysList.size(); i++) {
-            if (daysList.get(i) <= deployDay) {
-                count++;
-            } else {
-                result.add(count);
-                count = 1;
-                deployDay = daysList.get(i);
-            }
-        }
-        result.add(count);
+		for (int i = 1; i < daysList.size(); i++) {
+			if (daysList.get(i) <= deployDay) {
+				count++;
+			} else {
+				result.add(count);
+				count = 1;
+				deployDay = daysList.get(i);
+			}
+		}
+		result.add(count); // 마지막 count 쌓인 작업 출력
 
-        return result.stream().mapToInt(Integer::intValue).toArray();
+		return result.stream().mapToInt(Integer::intValue).toArray();
     }
 }

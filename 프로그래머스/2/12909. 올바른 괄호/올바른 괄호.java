168423b9086d10695851cea76a8898b70a4f09e5
박경탁 <68748397+DevKTak@users.noreply.github.com>
2024-06-15 18:14:44 +0900

    [level 2] Title: 올바른 괄호, Time: 16.48 ms, Memory: 53.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+      	Stack<Character> stack = new Stack<>();
+		char[] chars = s.toCharArray();
+
+		for (int i = 0; i < chars.length; i++) {
+			if (chars[i] == '(') {
+				stack.push(chars[i]);
+			} else {
+				if (stack.isEmpty()) {
+					return false;
+				} else {
+					stack.pop();
+				}
+			}
+		}
+		return stack.isEmpty();
+    }
+}

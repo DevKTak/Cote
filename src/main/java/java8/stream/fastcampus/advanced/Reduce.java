3c5846179078047bca4f9f40b43f46cbd7609384
ktpark <qkrrudxkr77@bodyfriend.co.kr>
2024-08-19 18:00:11 +0900

    feat: Stream Master

--- src/main/java/java8/stream/fastcampus/advanced/Reduce.java
+++ src/main/java/java8/stream/fastcampus/advanced/Reduce.java
@@ -13,12 +13,14 @@ public class Reduce {
 	public static void main(String[] args) {
 		List<Integer> numbers = Arrays.asList(1, 4, -2, -5, 3);
 		int sum = numbers.stream()
+			// 메소드 레퍼런스로 간단하게 표현하는 방법
 			// .reduce((x, y) -> x + y)
 			.reduce(Integer::sum)
 			.get();
 		System.out.println(sum);
 
 		int max = numbers.stream()
+			// 메소드 레퍼런스로 간단하게 표현하는 방법
 			// .reduce((x, y) -> x > y ? x : y)
 			// .reduce((x, y) -> Math.max(x, y))
 			.reduce(Math::max)
@@ -34,6 +36,13 @@ public class Reduce {
 			.map(Integer::parseInt)
 			.reduce(0, (x, y) -> x + y);
 		System.out.println(sumOfNumberStrList);
+
+		// mapToInt를 활용하여 int에 특화된 메서드 sum() 을 사용해서 푸는법
+		int sumOfNumberStrList1_1 = numberStrList.stream()
+			.mapToInt(Integer::parseInt).sum();
+		System.out.println("sumOfNumberStrList1_1 = " + sumOfNumberStrList1_1);
+
+		// 자주 쓰지 않는 방법 (이렇게도 쓴다정도)
 		int sumOfNumberStrList2 = numberStrList.stream()
 			.reduce(0, (number, str) -> number + Integer.parseInt(str), (num1, num2) -> num1 + num2);
 		System.out.println(sumOfNumberStrList2);
@@ -51,10 +60,18 @@ public class Reduce {
 			.setName("Charlie")
 			.setFriendUserIds(Arrays.asList(204, 205, 207));
 		List<User> users = Arrays.asList(user1, user2, user3);
+
+
 		int sumOfNumberOfFriends = users.stream()
 			.map(User::getFriendUserIds)
 			.map(List::size)
 			.reduce(0, (x, y) -> x + y);
+
+		// mapToInt을 활용하여 더하는 법
+		// int sumOfNumberOfFriends = users.stream()
+		// 	.map(User::getFriendUserIds)
+		// 	.mapToInt(List::size)
+		// 		.sum();
 		System.out.println(sumOfNumberOfFriends);
 
 		Order order1 = new Order()
@@ -76,5 +93,12 @@ public class Reduce {
 		List<Order> orders = Arrays.asList(order1, order2, order3);
 
 		// TODO: find the sum of amounts
+		// 오더라인들의 가격의 총 합
+		BigDecimal sumOfAmounts = orders.stream()
+			.map(Order::getOrderLines) // Stream<List<OrderLine>>
+			.flatMap(orderLines -> orderLines.stream()) // 오더라인들을 평평하게해서 스트림으로 만듬 Stream<OrderLine>
+			.map(orderLine -> orderLine.getAmount()) // Stream<BigDecimal>
+			.reduce(BigDecimal.ZERO, (bigDecimal, bigDecimal2) -> bigDecimal.add(bigDecimal2));
+		System.out.println("sumOfAmounts = " + sumOfAmounts);
 	}
 }
--- src/main/java/testPackage/movie/MovieMain.java
+++ src/main/java/testPackage/movie/MovieMain.java
@@ -1,121 +1,121 @@
-package testPackage.movie;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
-
-public class MovieMain {
-
-	private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-	public static void main(String[] args) throws IOException {
-		List<Movie> movies = new ArrayList<>();
-		int movieCount = 5;
-
-		/**
-		 *	1번 문제
-		 *	영화 입력 및 오름차순 출력
-		 */
-		inputMovies(movies, movieCount);
-		printMovies(movies);
-
-		/**
-		 * 	2번 문제
-		 *	영화 제목 입력 시 해당 영화 검색(이진탐색)하여 출력
-		 */
-		System.out.print("\n영화제목: ");
-		String searchTitle = br.readLine();
-
-		Movie movie = binarySearchMovie(searchTitle, movies).orElseThrow(
-			() -> new NoSuchElementException("해당 영화는 존재하지 않습니다."));
-
-		printMovieByTitle(movie);
-	}
-
-	private static void printMovieByTitle(Movie movie) {
-		System.out.println("\n영화제목 | 주인공 | 제작사 | 가격 | 등급");
-		System.out.println(
-			movie.getTitle() + " " + movie.getActor() + " " + movie.getCompany() + " " + movie.getPrice() + " "
-				+ movie.getGrade());
-	}
-
-	private static void printMovies(List<Movie> movies) {
-		Collections.sort(movies);
-		StringBuilder sb = new StringBuilder();
-
-		for (Movie movie : movies) {
-			sb.append(System.lineSeparator()).append("제목: ").append(movie.getTitle());
-			sb.append(System.lineSeparator()).append("주인공: ").append(movie.getActor());
-			sb.append(System.lineSeparator()).append("제작사: ").append(movie.getCompany());
-			sb.append(System.lineSeparator()).append("가격: ").append(movie.getPrice());
-			sb.append(System.lineSeparator()).append("등급: ").append(movie.getGrade());
-		}
-		System.out.println(sb);
-	}
-
-	private static void inputMovies(List<Movie> movies, int movieCount) throws IOException {
-		while (movieCount-- > 0) {
-			movies.add(Movie.builder()
-				.title(br.readLine())
-				.actor(br.readLine())
-				.company(br.readLine())
-				.price(Integer.parseInt(br.readLine()))
-				.grade(Integer.parseInt(br.readLine()))
-				.build());
-		}
-	}
-
-	private static Optional<Movie> binarySearchMovie(String searchTitle, List<Movie> movies) {
-		int left = 0;
-		int right = movies.size() - 1;
-		int mid;
-
-		while (left <= right) {
-			mid = (left + right) / 2;
-
-			if (searchTitle.compareTo(movies.get(mid).getTitle()) < 0) {
-				right = mid - 1;
-			} else if (searchTitle.compareTo(movies.get(mid).getTitle()) > 0) {
-				left = mid + 1;
-			} else {
-				return Optional.ofNullable(movies.get(mid));
-			}
-		}
-		return Optional.empty();
-	}
-}
-
-/*
-[TestCase]
-
-범죄도시3
-마동석
-CJ시네마
-18000
-15
-가가가
-가동석
-가시네마
-11111
-11
-라라라
-라동석
-라시네마
-44444
-44
-나나나
-나동석
-나시네마
-22222
-22
-다다다
-다동석
-다시네마
-33333
-33
- */
+// package testPackage.movie;
+//
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.util.ArrayList;
+// import java.util.Collections;
+// import java.util.List;
+// import java.util.NoSuchElementException;
+// import java.util.Optional;
+//
+// public class MovieMain {
+//
+// 	private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+// 	public static void main(String[] args) throws IOException {
+// 		List<Movie> movies = new ArrayList<>();
+// 		int movieCount = 5;
+//
+// 		/**
+// 		 *	1번 문제
+// 		 *	영화 입력 및 오름차순 출력
+// 		 */
+// 		inputMovies(movies, movieCount);
+// 		printMovies(movies);
+//
+// 		/**
+// 		 * 	2번 문제
+// 		 *	영화 제목 입력 시 해당 영화 검색(이진탐색)하여 출력
+// 		 */
+// 		System.out.print("\n영화제목: ");
+// 		String searchTitle = br.readLine();
+//
+// 		Movie movie = binarySearchMovie(searchTitle, movies).orElseThrow(
+// 			() -> new NoSuchElementException("해당 영화는 존재하지 않습니다."));
+//
+// 		printMovieByTitle(movie);
+// 	}
+//
+// 	private static void printMovieByTitle(Movie movie) {
+// 		System.out.println("\n영화제목 | 주인공 | 제작사 | 가격 | 등급");
+// 		System.out.println(
+// 			movie.getTitle() + " " + movie.getActor() + " " + movie.getCompany() + " " + movie.getPrice() + " "
+// 				+ movie.getGrade());
+// 	}
+//
+// 	private static void printMovies(List<Movie> movies) {
+// 		Collections.sort(movies);
+// 		StringBuilder sb = new StringBuilder();
+//
+// 		for (Movie movie : movies) {
+// 			sb.append(System.lineSeparator()).append("제목: ").append(movie.getTitle());
+// 			sb.append(System.lineSeparator()).append("주인공: ").append(movie.getActor());
+// 			sb.append(System.lineSeparator()).append("제작사: ").append(movie.getCompany());
+// 			sb.append(System.lineSeparator()).append("가격: ").append(movie.getPrice());
+// 			sb.append(System.lineSeparator()).append("등급: ").append(movie.getGrade());
+// 		}
+// 		System.out.println(sb);
+// 	}
+//
+// 	private static void inputMovies(List<Movie> movies, int movieCount) throws IOException {
+// 		while (movieCount-- > 0) {
+// 			movies.add(Movie.builder()
+// 				.title(br.readLine())
+// 				.actor(br.readLine())
+// 				.company(br.readLine())
+// 				.price(Integer.parseInt(br.readLine()))
+// 				.grade(Integer.parseInt(br.readLine()))
+// 				.build());
+// 		}
+// 	}
+//
+// 	private static Optional<Movie> binarySearchMovie(String searchTitle, List<Movie> movies) {
+// 		int left = 0;
+// 		int right = movies.size() - 1;
+// 		int mid;
+//
+// 		while (left <= right) {
+// 			mid = (left + right) / 2;
+//
+// 			if (searchTitle.compareTo(movies.get(mid).getTitle()) < 0) {
+// 				right = mid - 1;
+// 			} else if (searchTitle.compareTo(movies.get(mid).getTitle()) > 0) {
+// 				left = mid + 1;
+// 			} else {
+// 				return Optional.ofNullable(movies.get(mid));
+// 			}
+// 		}
+// 		return Optional.empty();
+// 	}
+// }
+//
+// /*
+// [TestCase]
+//
+// 범죄도시3
+// 마동석
+// CJ시네마
+// 18000
+// 15
+// 가가가
+// 가동석
+// 가시네마
+// 11111
+// 11
+// 라라라
+// 라동석
+// 라시네마
+// 44444
+// 44
+// 나나나
+// 나동석
+// 나시네마
+// 22222
+// 22
+// 다다다
+// 다동석
+// 다시네마
+// 33333
+// 33
+//  */
